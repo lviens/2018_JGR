fb0197abfc82f030d38825664b66f485e91a1222
Loic Viens <loicviens@fas.harvard.edu>
2018-04-19 18:43:54 -0400

    Create  Get_SC_functions.py

---
+++ Functions/Get_SC_functions.py
@@ -0,0 +1,42 @@
+"""
+L. Viens 02/29/2018
+This function computes Single-station Cross-correlation (SC) functions from the data recorded by the vertical and horizontal components at one seismic station.
+It is an example of the paper Viens et al. (2018) JGR.
+The SC functions are computed in the frequency domain.
+"""
+
+import numpy as np
+import obspy.signal as obssig
+
+def get_SC_functions(dat_Z, dat_HZ, delta, len_corr):
+        """
+    Function to get the day and month of the year from the day of the year.
+    INPUT: 
+        - dat_Z  = data recoded by the vertical component in time domain
+        - dat_HZ = data recoded by one horizontal component (either NS or EW) in time domain
+        - delta  = Sampling rate() in Hz)
+        - len_corr = Lenght of the SC function in second (as the causal and acausal parts of the SC function are computed, the total length in 2 times len_corr)
+    OUTPUT:
+        - corr = SC function in time domain.
+    """
+
+    n = len(dat_Z) 
+    dt = 1/delta
+    
+    # 1-bit normalization
+    dat_Z = np.sign(dat_Z)
+    dat_HZ = np.sign(dat_HZ)
+
+    # Compute FFT
+    fft_Z = np.fft.fft(dat_Z, n*4)
+    fft_HZ = np.fft.fft(dat_HZ, n*4)
+    sj2 = obssig.util.smooth(np.absolute(fft_Z), 20)
+
+    # Compute SC functions in Fourier domain and IFFT to return in the time domain
+    cc_t = np.real(np.fft.ifft( (fft_HZ * np.conj(fft_Z))/ (sj2**2) ))  
+    # Rearrange causal and acausal parts
+    corr_tot = np.concatenate((cc_t[len(cc_t)/2:], cc_t[:len(cc_t)/2]))
+    # Select the data between -len_corr and len_corr second around the zero time-lag
+    corr  = corr_tot[len(corr_tot)/2-len_corr*dt:len(corr_tot)/2+len_corr*dt]
+
+    return corr
